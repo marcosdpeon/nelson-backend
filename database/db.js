da8a5d93ef25f0b29f28857ac67142658696b60d
light7dream <ry.mc.le.92@gmail.com>
2023-05-22 16:32:17 -0700

    Update db.js

--- database/db.js
+++ database/db.js
@@ -1,3 +1,4 @@
 module.exports = {
-  db: 'mongodb://localhost:27017/People',
+  // db: 'mongodb://localhost:27017/People',
+  db: 'mongodb+srv://marcosdpeon:jeupIs1QORObX1z7@cluster0.vorzpr1.mongodb.net/nelson',
 }
